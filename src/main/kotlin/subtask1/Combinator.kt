6b99d556c24284ebb57abbfa142183153e033291
fusion-steel <pavelgently@gmail.com>
2021-07-06 22:32:49 +0300

    feat: checkChooseFromArray method implementation.

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,40 @@
 package subtask1
 
+import java.lang.IllegalArgumentException
+
 class Combinator {
 
-    // TODO: Complete the following function
+    /**
+     * This method finds k from binomial coefficient formula
+     */
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val n = array[0]
+        val k = array[1]
+
+        var res: Int? = null
+
+        for (i in 1..k) {
+            val currentCombination = combination(k, i)
+            if (currentCombination == n) {
+                res = i
+                break
+            }
+        }
+        return res
+    }
+
+    private fun Int.factorial(): Long {
+        var i = 1
+        var factorial: Long = 1
+        while (i <= this) {
+            factorial *= i.toLong()
+            i++
+        }
+        return factorial
+    }
+
+    private fun combination(n: Int, k: Int): Int {
+        if (n < k) throw IllegalArgumentException("n should be bigger than k")
+        return (n.factorial() / (k.factorial() * (n - k).factorial())).toInt()
     }
 }
