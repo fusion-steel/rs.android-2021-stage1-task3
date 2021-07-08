5523fef231f7612a5b4c4a5062a9009ea6b34a67
fusion-steel <lethal@mail.ua>
2021-07-08 12:12:24 +0300

    feat: decomposeNumber implementation.

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,38 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
+    /**
+     * This method decompose square of a number to the sum of squares
+     */
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number < 5) return null
+        return decompose(sqr(number), number - 1)
     }
+
+    private fun decompose(sqr: Int, reducedNumber: Int): Array<Int>? {
+        for (i in reducedNumber downTo 1) {
+            val rest = sqr - sqr(i)
+
+            if (rest == 0) {
+                return arrayOf(i)
+            } else if (rest < 0) {
+                return null
+            }
+
+            val possibleNext = sqrt(rest.toDouble()).toInt()
+            val nextVal = if (possibleNext >= i) {
+                decompose(rest, i - 1)
+            } else {
+                decompose(rest, possibleNext)
+            }
+            if (!nextVal.isNullOrEmpty())
+                return nextVal + arrayOf(i)
+        }
+        return null
+    }
+
+    private fun sqr(n: Int) = n * n
 }
